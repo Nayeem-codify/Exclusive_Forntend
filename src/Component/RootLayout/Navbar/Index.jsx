cabfbc2b18e7588d2b01c813034b61cb60596ef8
usernameNayeem-codify <useremailwebdevlopment2306@gmail.com>
2024-12-01 20:01:17 +0600

    usermenu

--- src/Component/RootLayout/Navbar/Index.jsx
+++ src/Component/RootLayout/Navbar/Index.jsx
@@ -2,7 +2,9 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import { FiSearch } from "react-icons/fi";
 import { IoMdHeartEmpty } from "react-icons/io";
-import { FaCartArrowDown, FaRegUserCircle } from "react-icons/fa";
+import { FaCartArrowDown, FaUser } from "react-icons/fa";
+import { CiLogout, CiStar, CiUser, CiWallet } from "react-icons/ci";
+import { MdOutlineCollections } from "react-icons/md";
 
 const Navbar = () => {
   const navItem = [
@@ -63,16 +65,48 @@ const Navbar = () => {
                 <FiSearch />
               </span>
             </div>
-            <div className="flex items-center gap-x-5 ">
-              <span className="bg-text-text_black000000 text-2xl">
+            <div className="flex items-center gap-x-[20px] ">
+              <span className="bg-text-text_black000000 text-2xl cursor-pointer">
                 <IoMdHeartEmpty />
               </span>
-              <span className="bg-text-text_black000000 text-2xl amount">
+              <span className="bg-text-text_black000000 text-2xl amount cursor-pointer">
                 <FaCartArrowDown />
               </span>
-              <span className="bg-text-text_black000000 text-2xl">
-                <FaRegUserCircle />
+              <span className="text-text_whiteFAFAFA text-xl rounded-full bg-red-600 p-2 cursor-pointer relative">
+                <FaUser />
               </span>
+              <div className="absolute top-0 left-0 bg-gray-200 px-10 flex flex-col gap-y-3 py-5">
+                <div className="flex items-center gap-x-2 ">
+                  <span className="text-text_whiteFAFAFA text-2xl">
+                    <CiUser />
+                  </span>
+                  <h3 className="text-text_whiteFAFAFA font-poppins font-normal text-xl">Manage My Account</h3>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  <span className="text-text_whiteFAFAFA text-2xl">
+                    <CiWallet />
+                  </span>
+                  <h3 className="text-text_whiteFAFAFA font-poppins font-normal text-xl">My Order</h3>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  <span className="text-text_whiteFAFAFA text-2xl">
+                    <MdOutlineCollections />
+                  </span>
+                  <h3 className="text-text_whiteFAFAFA font-poppins font-normal text-xl">My Cancellations</h3>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  <span className="text-text_whiteFAFAFA text-2xl">
+                    <CiStar />
+                  </span>
+                  <h3 className="text-text_whiteFAFAFA font-poppins font-normal text-xl">My Reviews</h3>
+                </div>
+                <div className="flex items-center gap-x-2">
+                  <span className="text-text_whiteFAFAFA text-2xl">
+                    <CiLogout />
+                  </span>
+                  <h3 className="text-text_whiteFAFAFA font-poppins font-normal text-xl">Logout</h3>
+                </div>
+              </div>
             </div>
           </div>
         </div>
